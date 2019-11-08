5ab629a0c6264867d935d6ed87bb1fc2c20a3abd
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-11-08 12:47:55 -0700

    added backgrounds. button effects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,74 @@
-$(document).ready(function() {
-    console.log( "ready to search dem animalz!" );
-  
+$(document).ready(function () {
 
-var animalArray = ["naked mole rat", "mantis shrimp", "vampire squid", "orchid mantis",];
+    //global variables
+    var animalArray = ["Climate Change Denial", "Anti-Vax", "Roswell", "Vaccines Cause Autism", "Fluoridation Plot", "Faked Moon Landing", "New World Order", "Denver Airport Conspiracy", "Chemtrails", "Ancient Aliens", "Illuminati",];
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="
+    var apiKey = "&api_key=Rxfl45R7o45YqIKgQLTGgQ6sYL4inFjm&limit=10";
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q="
+    //goes through var animalArray and makes a button for each value.
+    function makeButtons() {
+        $("#aButton").empty();
+        for (var i = 0; i < animalArray.length; i++) {
+            var but = $('<button class="buttons glow-on-hover">' + (animalArray[i]) + '</button>');
+            but.addClass("animalButton");
+            but.attr("data", animalArray[i]);
+            $("#aButton").append(but);
+        }
+    }
 
-var apiKey = "&api_key=Rxfl45R7o45YqIKgQLTGgQ6sYL4inFjm";
+    //calls function for inital array of buttons
+    makeButtons();
 
-function makeButtons(){
-    for (var i=0; i<animalArray.length; i++) {
-        var but = $('<button class=buttons>'+(animalArray[i])+'</button>').click(function () {
-            console.log('hi');
-            $.ajax({
-                url: (queryURL)+(animalArray[i])+(apiKey),
-                method: "GET",
-            }).then(function(response) {
-                console.log(response);
+    //puts search element into button array above
+    $("#aSubmit").click(function (event) {
+        event.preventDefault();
+        var animal = $("#aSearch").val();
+        animalArray.push(animal);
+        makeButtons();
+    });
+
+    //pulls gifs from Giphy
+    $(document).on("click", ".animalButton", function () {
+        $.ajax({
+
+//PROBLEM BELOW HERE!!!
+            url: (queryURL) + ("Vaccines Cause Autism") + (apiKey),
+//PROBLEM ABOVE HERE!!!
+
+            method: "GET",
+        }).then(function (response) {
+            results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                //local variables for gifs
+                var gifDiv = $("<div class='gifs'>");
+                var gRating = results[i].rating;
+                var p = $("<p>").text("Rating: " + gRating);
+                var animalImage = $("<img>");
+                animalImage.addClass("gif");
+                animalImage.attr("src", results[i].images.fixed_height.url);
+                animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+                animalImage.attr("data-animate", results[i].images.fixed_height.url);
+//BUGS! BUGS! BUGS!
+                animalImage.attr("data-state", "still");
+//BUGS! BUGS! BUGS
+                gifDiv.prepend(p);
+                gifDiv.prepend(animalImage);
+                $("#aGifs").prepend(gifDiv);
+            };
+
+
+            $(".gif").on("click", function () {
+                var state = $(this).attr("data-state");
+                console.log(this);
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
             });
         });
-        $("#aButton").append(but);
-    }
-}
-makeButtons();
-
-$("#aSubmit").click(function(event){
-    event.preventDefault();
-    var animal=$("#aSearch").val();
-    animalArray.push(animal);
-    console.log(animalArray);
-});
-
+    });
 });

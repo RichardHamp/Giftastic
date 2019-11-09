db9f0b65e47476fcfc1249ca87a224cdb869266d
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-11-08 18:32:34 -0700

    fixed bug that had images start animated. fixed bug that would not bring up proper gifs. Wrote ReadMe.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Giftastic
+# Giftastic: Shit my Coworker Max Believes
+
+My coworker, Max, is a giant conspiracy nut. Emphasis on the nut. Explore all the crazy he believes by looking up gifs for alien abductions and the New World Order.
+
+# How To Play:
+
+You click on any of the conspiracy buttons at the top of the page. This will populate ten gifs for your viewing pleasure. You may add additional buttons by writing what you want to look for in the search bar and pressing the "into the crazy" button. 
+
+# Demo:
+
+Check it out here: https://fozzie77.github.io/Giftastic/
+
+# Build Tools:
+
+* Bootstrap
+* JQuery
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
 
     //global variables
     var crazyArray = ["Climate Change Denial", "Anti-Vax", "Roswell", "Vaccines Cause Autism", "Faked Moon Landing", "Chemtrails", "Ancient Aliens", "Illuminati",];
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="
-    var apiKey = "&api_key=Rxfl45R7o45YqIKgQLTGgQ6sYL4inFjm&limit=10";
+   
 
     //goes through var crazyArray and makes a button for each value.
     function makeButtons() {
@@ -11,7 +10,7 @@ $(document).ready(function () {
         for (var i = 0; i < crazyArray.length; i++) {
             var but = $('<button class="buttons glow-on-hover">' + (crazyArray[i]) + '</button>');
             but.addClass("crazyButton");
-            but.attr("data", crazyArray[i]);
+            but.attr("value", crazyArray[i]);
             $("#aButton").prepend(but);
         }
     }
@@ -21,6 +20,7 @@ $(document).ready(function () {
 
     //puts search element into button array above
     $("#aSubmit").click(function (event) {
+        
         event.preventDefault();
         var crazy = $("#aSearch").val();
         crazyArray.push(crazy);
@@ -29,11 +29,17 @@ $(document).ready(function () {
 
     //pulls gifs from Giphy
     $(document).on("click", ".crazyButton", function () {
+    var work=this.value;
+    console.log(work);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
+    +work+"&api_key=Rxfl45R7o45YqIKgQLTGgQ6sYL4inFjm&limit=10"
+        
         $("#aGifs").empty();
+        console.log(queryURL);
         $.ajax({
 
 //PROBLEM BELOW HERE!!!
-            url: (queryURL) + (this.data) + (apiKey),
+            url: (queryURL),
 //PROBLEM ABOVE HERE!!!
 
             method: "GET",
@@ -46,12 +52,10 @@ $(document).ready(function () {
                 var p = $("<p>").text("Rating: " + gRating);
                 var crazyImage = $("<img>");
                 crazyImage.addClass("gif");
-                crazyImage.attr("src", results[i].images.fixed_height.url);
+                crazyImage.attr("src", results[i].images.fixed_height_still.url);
                 crazyImage.attr("data-still", results[i].images.fixed_height_still.url);
                 crazyImage.attr("data-animate", results[i].images.fixed_height.url);
-//BUGS! BUGS! BUGS!
                 crazyImage.attr("data-state", "still");
-//BUGS! BUGS! BUGS
                 gifDiv.prepend(p);
                 gifDiv.prepend(crazyImage);
                 $("#aGifs").prepend(gifDiv);
